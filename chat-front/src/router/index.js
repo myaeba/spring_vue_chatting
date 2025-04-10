b7cf51c4a290c6c665db2cf8e31ae578c0799ef7
myaeba <lim104321@naver.com>
2025-04-10 23:14:27 +0900

    순수 웹소켓 - 프론트

--- chat-front/src/router/index.js
+++ chat-front/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import MemberCreate from "@/views/MemberCreate.vue";
 import LoginPage from "@/views/LoginPage.vue";
 import MemberList from "@/views/MemberList.vue";
+import SimpleWebsocket from "@/views/SimpleWebsocket.vue";
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: "/member/list",
     name: "MemberList",
     component: MemberList,
+  },
+  {
+    path: "/simple/chat",
+    name: "SimpleWebsocket",
+    component: SimpleWebsocket,
   },
 ];
 
