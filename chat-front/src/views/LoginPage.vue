dcd8472ba8be515b93541ddf31095630346c6eb3
myaeba <lim104321@naver.com>
2025-04-10 20:55:26 +0900

    로그인 화면 작성

--- chat-front/src/views/LoginPage.vue
+++ chat-front/src/views/LoginPage.vue
@@ -3,10 +3,9 @@
     <v-row justify="center">
       <v-col cols="12" sm="4" md="6">
         <v-card>
-          <v-card-title class="text-h5 text-center">회원가입</v-card-title>
+          <v-card-title class="text-h5 text-center">로그인</v-card-title>
           <v-card-text>
-            <v-form @submit.prevent="memberCreate">
-              <v-text-field label="이름" v-model="name" required></v-text-field>
+            <v-form @submit.prevent="doLogin">
               <v-text-field
                 label="email"
                 v-model="email"
@@ -19,7 +18,7 @@
                 type="password"
                 required
               ></v-text-field>
-              <v-btn type="submit" color="primary" block>등록</v-btn>
+              <v-btn type="submit" color="primary" block>로그인</v-btn>
             </v-form>
           </v-card-text>
         </v-card>
@@ -29,26 +28,29 @@
 </template>
 <script>
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 export default {
   data() {
     return {
-      name: "",
       email: "",
       password: "",
     };
   },
   methods: {
-    async memberCreate() {
-      const data = {
-        name: this.name,
-        email: this.email,
-        password: this.password,
-      };
-      await axios.post(
-        `${process.env.VUE_APP_API_BASE_URL}/member/create`,
-        data
+    async doLogin() {
+      const loginData = { email: this.email, password: this.password };
+      const response = await axios.post(
+        `${process.env.VUE_APP_API_BASE_URL}/member/doLogin`,
+        loginData
       );
-      this.$router.push("/");
+      console.log(response);
+      const token = response.data.token;
+      const role = jwtDecode(token).role;
+      const email = jwtDecode(token).sub;
+      localStorage.setItem("token", token);
+      localStorage.setItem("role", role);
+      localStorage.setItem("email", email);
+      window.location.href = "/";
     },
   },
 };
