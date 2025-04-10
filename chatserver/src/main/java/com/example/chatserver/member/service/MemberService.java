2d117ad02c4f815a6e6535fce0631b9cbd56b8d3
myaeba <lim104321@naver.com>
2025-04-10 17:26:35 +0900

    jwt 토큰 검증

--- chatserver/src/main/java/com/example/chatserver/member/service/MemberService.java
+++ chatserver/src/main/java/com/example/chatserver/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.example.chatserver.member.service;
 
 import com.example.chatserver.member.domain.Member;
+import com.example.chatserver.member.dto.MemberListResDto;
 import com.example.chatserver.member.dto.MemberLoginReqDto;
 import com.example.chatserver.member.dto.MemberSaveReqDto;
 import com.example.chatserver.member.repository.MemberRepository;
@@ -8,6 +9,9 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -44,5 +48,20 @@ public class MemberService {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
         return member;
+    }
+
+    public List<MemberListResDto> findAll() {
+        List<Member> members = memberRepository.findAll();
+        List<MemberListResDto> memberListResDtos = new ArrayList<>();
+
+        for (Member member : members) {
+            MemberListResDto memberListResDto = new MemberListResDto();
+            memberListResDto.setId(member.getId());
+            memberListResDto.setEmail(member.getEmail());
+            memberListResDto.setName(member.getName());
+
+            memberListResDtos.add(memberListResDto);
+        }
+        return memberListResDtos;
     }
 }
