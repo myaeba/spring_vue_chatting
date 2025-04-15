7fbd74cc94e658d52138244f8589c66c35a153d8
myaeba <lim104321@naver.com>
2025-04-15 20:16:03 +0900

    stomp 채팅 화면

--- chat-front/src/router/index.js
+++ chat-front/src/router/index.js
@@ -3,6 +3,7 @@ import MemberCreate from "@/views/MemberCreate.vue";
 import LoginPage from "@/views/LoginPage.vue";
 import MemberList from "@/views/MemberList.vue";
 import SimpleWebsocket from "@/views/SimpleWebsocket.vue";
+import StompChatPage from "@/views/StompChatPage.vue";
 
 const routes = [
   {
@@ -24,6 +25,11 @@ const routes = [
     path: "/simple/chat",
     name: "SimpleWebsocket",
     component: SimpleWebsocket,
+  },
+  {
+    path: "/chatpage/:roomId",
+    name: "StompChatPage",
+    component: StompChatPage,
   },
 ];
 
